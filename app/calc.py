156324f44643e94bd4f3c090a4086edc145b0358
Andy Fisher <ajf@howitworks.co.uk>
2020-05-21 17:39:32 +0100

    Added custom user model

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,8 @@
 def add(x, y):
     """Add 2 numbers together"""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
-# from calc import add   # works
-from app.calc import add # fails
+from calc import add   # works
+# from app.calc import add # fails
+from calc import subtract
 
 
 class CalcTests(TestCase):
@@ -9,3 +10,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that 2 numbers are added together"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtrated and returned"""
+        self.assertEqual(subtract(5, 11), 6)
