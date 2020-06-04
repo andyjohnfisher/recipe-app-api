703301dc27c3d3c5b381f5f674637c1acb9fb79d
Andy Fisher <ajf@howitworks.co.uk>
2020-06-04 18:19:06 +0100

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -9,6 +9,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('reipies', views.RecipeViewSet)
 
 app_name = 'recipe'
 
